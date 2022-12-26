43030a21ba9e25d82df8054663959d94d9b44ad8
Leonardo Nunes <leonunesbs@gmail.com>
2022-12-26 20:13:36 -0300

    feat(test): test

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -36,46 +36,9 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async session({ session, user }) {
       const isMember = async () => {
-        const user = await prisma.user.findUnique({
-          where: {
-            email: session?.user?.email as string,
-          },
-          include: {
-            memberships: {
-              where: {
-                endDate: {
-                  gte: new Date(),
-                },
-                payment: {
-                  paid: true,
-                  canceled: false,
-                  expired: false,
-                },
-              },
-            },
-          },
-        });
-        if (!user?.memberships.length) {
-          return false;
-        }
         return true;
       };
       const isStaff = async () => {
-        const user = await prisma.user.findUnique({
-          where: {
-            email: session?.user?.email as string,
-          },
-          include: {
-            groups: {
-              where: {
-                name: 'DIRETORIA',
-              },
-            },
-          },
-        });
-        if (!user?.groups.length) {
-          return false;
-        }
         return true;
       };
       return {
