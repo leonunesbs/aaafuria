2b46606142e483b0f46a10176ac1b4cd69958d5f
Leonardo Nunes <leonunesbs@gmail.com>
2022-12-27 11:05:25 -0300

    feat(page): clean page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,5 @@
-import { GetServerSideProps } from 'next';
 import { Layout } from '@/components/templates';
-import { authOptions } from './api/auth/[...nextauth]';
-import { unstable_getServerSession } from 'next-auth';
 
 export default function Home() {
   return <Layout title="Início">Teste</Layout>;
 }
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const session = await unstable_getServerSession(
-    ctx.req,
-    ctx.res,
-    authOptions,
-  );
-  if (!session) {
-    return {
-      redirect: {
-        destination: `/auth/login?callbackUrl=${ctx.resolvedUrl}`,
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {
-      session,
-    },
-  };
-};
