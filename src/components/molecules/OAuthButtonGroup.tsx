023e4bff1dd52f50c74134e7916b3295cbd33f21
Leonardo Nunes <leonunesbs@gmail.com>
2022-12-27 01:20:44 -0300

    feat(ui): loading

--- src/components/molecules/OAuthButtonGroup.tsx
+++ src/components/molecules/OAuthButtonGroup.tsx
@@ -9,37 +9,42 @@ interface OAuthButtonGroupProps {
   children?: ReactNode;
 }
 
-export function OAuthButtonGroup({}: OAuthButtonGroupProps) {
+const OAuthButton = ({ name, icon }: { name: string; icon: JSX.Element }) => {
   const router = useRouter();
   const { callbackUrl } = router.query;
-  const providers = [
-    { name: 'Google', icon: <GoogleIcon boxSize="5" /> },
-    { name: 'GitHub', icon: <GitHubIcon boxSize="5" /> },
-  ];
   const [loading, setLoading] = useState(false);
 
   const handleSignIn = async (provider: string) => {
     setLoading(true);
     await signIn(provider, {
       callbackUrl: callbackUrl ? (callbackUrl as string) : undefined,
-    }).then(() => setLoading(false));
+    }).finally(() => setLoading(false));
   };
+  return (
+    <Button
+      variant="outline"
+      fontWeight={'normal'}
+      width="full"
+      onClick={() => handleSignIn(name.toLowerCase())}
+      leftIcon={icon}
+      isLoading={loading}
+      loadingText={'Carregando...'}
+    >
+      <Text>Entrar com {name}</Text>
+    </Button>
+  );
+};
+
+export function OAuthButtonGroup({}: OAuthButtonGroupProps) {
+  const providers = [
+    { name: 'Google', icon: <GoogleIcon boxSize="5" /> },
+    { name: 'GitHub', icon: <GitHubIcon boxSize="5" /> },
+  ];
 
   return (
     <Stack spacing="4" width="full">
       {providers.map(({ name, icon }) => (
-        <Button
-          key={name}
-          variant="outline"
-          fontWeight={'normal'}
-          width="full"
-          onClick={() => handleSignIn(name.toLowerCase())}
-          leftIcon={icon}
-          isLoading={loading}
-          loadingText={'Carregando...'}
-        >
-          <Text>Entrar com {name}</Text>
-        </Button>
+        <OAuthButton key={name} name={name} icon={icon} />
       ))}
     </Stack>
   );
