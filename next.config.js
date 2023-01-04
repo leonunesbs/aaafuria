eb89dabf91ba1dec4ee13716261cd92984038877
Leonardo Nunes <leonunesbs@gmail.com>
2023-01-04 19:50:43 -0300

    feat(pwa): disable

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  disable: true,
+});
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
