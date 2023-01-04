485f06fcc0987d6d7f7284ca3b5c627c29133663
Leonardo Nunes <leonunesbs@gmail.com>
2023-01-04 19:17:32 -0300

    feat(add): cache

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -21,7 +21,13 @@ function getBaseUrl() {
 }
 
 export const trpc = createTRPCNext<AppRouter>({
-  config({}) {
+  config({ ctx }) {
+    // optional: use SSG-caching for each rendered page (see caching section for more details)
+    const ONE_DAY_SECONDS = 60 * 60 * 24;
+    ctx?.res?.setHeader(
+      'Cache-Control',
+      `s-maxage=1, stale-while-revalidate=${ONE_DAY_SECONDS}`,
+    );
     return {
       links: [
         httpBatchLink({
@@ -30,6 +36,24 @@ export const trpc = createTRPCNext<AppRouter>({
            * @link https://trpc.io/docs/ssr
            **/
           url: `${getBaseUrl()}/api/trpc`,
+          headers() {
+            if (ctx?.req) {
+              // To use SSR properly, you need to forward the client's headers to the server
+              // This is so you can pass through things like cookies when we're server-side rendering
+              // If you're using Node 18, omit the "connection" header
+              const {
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                ...headers
+              } = ctx.req.headers;
+
+              return {
+                ...headers,
+                // Optional: inform server that it's an SSR request
+                'x-ssr': '1',
+              };
+            }
+            return {};
+          },
         }),
       ],
       /**
