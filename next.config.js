76de6e4328a509802b8c7a887c807efe6bc0d557
Leonardo Nunes <leonunesbs@gmail.com>
2023-01-04 19:54:45 -0300

    feat(pwa): activate

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  disable: true,
+  disable: process.env.NODE_ENV === 'development',
 });
 const nextConfig = {
   reactStrictMode: true,
