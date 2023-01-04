2d0f251e24e3ce8ffd6016c08f7c665817ade1c5
Leonardo Nunes <leonunesbs@gmail.com>
2023-01-04 12:29:37 -0300

    feat(add): seo

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -17,10 +17,13 @@ import { ReactNode, useContext } from 'react';
 import { ColorContext } from '@/contexts';
 import Head from 'next/head';
 import { trpc } from '@/utils/trpc';
+import { useRouter } from 'next/router';
 
 interface LayoutProps {
   children: ReactNode;
   title: string;
+  description?: string;
+  keywords?: string;
   subHeader?: ReactNode;
   headerProps?: BoxProps;
   containerProps?: ContainerProps;
@@ -53,6 +56,8 @@ const StaffGate = ({ isLoading }: { isLoading: boolean }) => {
 
 export function Layout({
   title,
+  description,
+  keywords,
   children,
   subHeader,
   headerProps,
@@ -60,6 +65,7 @@ export function Layout({
   footerProps,
   staffCheck = false,
 }: LayoutProps) {
+  const router = useRouter();
   const { data: isStaff, isLoading } = trpc.auth.isStaff.useQuery(undefined, {
     enabled: staffCheck,
   });
@@ -67,8 +73,66 @@ export function Layout({
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
         <title>{`${title} | @aaafuria`}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta
+          name="description"
+          content={
+            description
+              ? `${description}`
+              : 'Plataforma de sócios e loja da Associação Atlética de Medicina Fúria Uniniovafapi. Seja sócio da Maior do Piauí e aproveite dos nossos produtos, treinos, ensaios, eventos e mais...'
+          }
+        />
+        <meta
+          name="keywords"
+          content={`aaafuria, site, atlética, fúria, medicina, loja, eventos, intermed, ${keywords}`}
+        />
+        <link rel="canonical" href={`https://aaafuria.site${router.asPath}`} />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={`https://aaafuria.site${router.asPath}`}
+        />
+        <meta
+          property="og:title"
+          content={title ? `@aaafuria | ${title}` : '@aaafuria'}
+        />
+        <meta
+          property="og:description"
+          content={
+            description
+              ? `${description}`
+              : 'Plataforma de sócios e loja da Associação Atlética de Medicina Fúria Uniniovafapi. Seja sócio da Maior do Piauí e aproveite dos nossos produtos, treinos, ensaios, eventos e mais...'
+          }
+        />
+        <meta property="og:image" content={'/logo-aaafuria.png'} />
+        <meta property="og:image:alt" content="logo" />
+        <meta property="og:locale" content="pt_BR" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content={`https://aaafuria.site${router.asPath}`}
+        />
+        <meta
+          property="twitter:title"
+          content={title ? `@aaafuria | ${title}` : '@aaafuria'}
+        />
+        <meta
+          property="twitter:description"
+          content={
+            description
+              ? `${description}`
+              : 'Plataforma de sócios e loja da Associação Atlética de Medicina Fúria Uniniovafapi. Seja sócio da Maior do Piauí e aproveite dos nossos produtos, treinos, ensaios, eventos e mais...'
+          }
+        />
+        <meta property="twitter:image" content={'/logo-aaafuria.png'} />
+        <meta name="twitter:image:alt" content="logo" />
       </Head>
       <Box minH="100vh">
         <Header {...headerProps} />
