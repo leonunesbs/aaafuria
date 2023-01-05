0dc2369fb0c2e85026cb6a9dac089a34016bd0e4
Leonardo Nunes <leonunesbs@gmail.com>
2023-01-05 11:43:37 -0300

    fix(url): fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.VERCEL_URL || 'http://localhost:3000',
+  siteUrl: process.env.WEBSITE_URL || 'http://localhost:3000',
   generateRobotsTxt: true,
   exclude: ['/server-sitemap.xml', '/404', '/403', '/admin/*'],
   robotsTxtOptions: {
     additionalSitemaps: [
-      `${process.env.VERCEL_URL || 'http://localhost:3000'}/server-sitemap.xml`, // <==== Add here
+      `${
+        process.env.WEBSITE_URL || 'http://localhost:3000'
+      }/server-sitemap.xml`, // <==== Add here
     ],
   },
 };
