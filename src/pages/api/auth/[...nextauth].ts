8503c11ba71b321211e2edc9fd618a64d33753f5
Leonardo Nunes <leonunesbs@gmail.com>
2023-01-05 02:14:05 -0300

    feat(ui): remove instagram

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,7 +4,6 @@ import EmailProvider from 'next-auth/providers/email';
 import FacebookProvider from 'next-auth/providers/facebook';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import InstagramProvider from 'next-auth/providers/instagram';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { appRouter } from '@/server/routers/_app';
 import { prisma } from '@/server/prisma';
@@ -34,10 +33,6 @@ export const authOptions: NextAuthOptions = {
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID as string,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
-    }),
-    InstagramProvider({
-      clientId: process.env.INSTAGRAM_CLIENT_ID as string,
-      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET as string,
     }),
   ],
   pages: {
