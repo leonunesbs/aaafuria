4eb5b05a5be97efd6cda2eddd1bef14857f12694
Leonardo Nunes <leonunesbs@gmail.com>
2023-01-05 11:31:31 -0300

    feat(add): sitemap

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  disable: true,
-});
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
