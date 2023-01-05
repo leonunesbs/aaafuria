55d5c0253a66290802b4782ce8fc65a8ebe78005
Leonardo Nunes <leonunesbs@gmail.com>
2023-01-05 12:15:54 -0300

    feat(fonts): reduce fonts

--- src/components/atoms/Fonts.tsx
+++ src/components/atoms/Fonts.tsx
@@ -26,17 +26,6 @@ export function Fonts({}: FontsProps) {
       /* latin */
       @font-face {
         font-family: 'Lato';
-        font-style: light;
-        font-weight: 300;
-        font-display: swap;
-        src: local('Lato Font'),
-        url('/fonts/woff2/Lato-Light.woff2') format('woff2'), /* will be preloaded */
-        url('/fonts/woff/Lato-Light.woff') format('woff'),
-        url('/fonts/Lato-Light.ttf') format('truetype');
-        unicode-range: U+000-5FF;
-      }
-      @font-face {
-        font-family: 'Lato';
         font-style: regular;
         font-weight: 400;
         font-display: swap;
@@ -55,17 +44,6 @@ export function Fonts({}: FontsProps) {
         url('/fonts/woff2/Lato-Bold.woff2') format('woff2'), /* will be preloaded */
         url('/fonts/woff/Lato-Bold.woff') format('woff'),
         url('/fonts/Lato-Bold.ttf') format('truetype');
-        unicode-range: U+000-5FF;
-      }
-      @font-face {
-        font-family: 'Lato';
-        font-style: extra-bold;
-        font-weight: 900;
-        font-display: swap;
-        src: local('Lato Font'),
-        url('/fonts/woff2/Lato-Black.woff2') format('woff2'), /* will be preloaded */
-        url('/fonts/woff/Lato-Black.woff') format('woff'),
-        url('/fonts/Lato-Black.ttf') format('truetype');
         unicode-range: U+000-5FF;
       }
       `}
